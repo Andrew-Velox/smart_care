85d0b4ca1c9e061202a84780401b81daac111a1c
andrew-velox <mohabbat.bd2020@gmail.com>
2025-07-22 20:48:44 +0600

    deployment on railway

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -31,7 +31,10 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://smart-care-q1sy.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = [
+    'https://smart-care-q1sy.onrender.com','https://*.127.0.0.1',
+    'https://smartcare-production.up.railway.app',
+    ]
 
 
 
